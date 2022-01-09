820a523c55918ddf4f249383e2a3acd848e1eaf5
Andres <agdevices@gmail.com>
2022-01-09 09:37:06 -0300

    Added db user - Set db port - Added env variables

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require(`express`);
 const router = express.Router();
 const cors = require('cors')
-const port = 3001;
+const port = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json())
--- db/mongodb.js
+++ db/mongodb.js
@@ -2,9 +2,12 @@ const mongodb = require('mongodb')
 
 const MongoClient = mongodb.MongoClient
 
+const dbUser = process.env.dbUser;
+const dbPass = process.env.dbPass;
+
 // const url = 'mongodb://localhost:27017'
 
-const url = `mongodb+srv://agdevices:Orion4923@cluster0.gohnb.mongodb.net/ucutcnc?retryWrites=true&w=majority`
+const url = `mongodb+srv://${dbUser}:${dbPass}@cluster0.gohnb.mongodb.net/ucutcnc?retryWrites=true&w=majority`
 
 
 function getDBClient (callback){
